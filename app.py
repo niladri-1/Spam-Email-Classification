d65fafbae707d89610cf78a3688bf03e9b98118b
Niladri Chatterjee <niladrichatterjee365@gmail.com>
2023-10-17 09:36:22 +0530

    Modify Home, LogIn, SignUp, About pages

--- app.py
+++ app.py
@@ -40,6 +40,10 @@ def transform_text(text):
 def home():
     return render_template('home.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 @app.route('/index')
 def index():
     return render_template('index.html')
